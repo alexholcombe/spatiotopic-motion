ad6fa00821fff938b3da9373425a06960fd830ee
Alex O. Holcombe <aoholcombe@gmail.com>
2014-11-21 08:37:54 +1100

    getting results dataframes to hook to thisTrial

--- dataframesTest.py
+++ dataframesTest.py
@@ -1,7 +1,22 @@
 from pandas import DataFrame
+from psychopy import data
 
 stimList=[]
 stimList.append({'location': 3, 'startLeft':False, 'upDown': 1, 'tilt': -2, 'jitter': .65})
+blockReps = 1
+trials = data.TrialHandler(stimList, blockReps)
+thisTrial = trials.next()
+print(thisTrial)
+print(type(thisTrial)) #psychopy data trialType
+fields = list(thisTrial)
+print(fields)
+#df = DataFrame(thisTrial)
+#df = DataFrame(dict(thisTrial)) 
+#df = DataFrame(thisTrial, columns = fields) #doesn't work. have to specify index (rownames)
+df = DataFrame(thisTrial, index=[1],
+                            columns = ['jitter','location','startLeft','tilt','upDown']) #columns included purely to specify their order
+df['respFwdBackslash']=1 #adds new column
+
 
 df= DataFrame({'tilt': [-3], 'respFwdBackslash':[1]}, 
                             columns=['tilt','respFwdBackslash']) #specifying the column names just to specify their order
