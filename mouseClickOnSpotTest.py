851bf590151b1907abf896cd1d81fd2ac9e2529a
Alex O. Holcombe <aoholcombe@gmail.com>
2014-11-27 11:30:13 +1100

    added file

---
+++ mouseClickOnSpotTest.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+from psychopy import sound, monitors, core, visual, event, data, gui, logging, info
+import numpy as np
+
+#Im supposed to use the built-in logging function, should be able to get everything out of that
+myMouse = event.Mouse(visible = 'true',win=myWin)
