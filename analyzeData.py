2d496bc00be4aeda31be400fd24414e9511f03cb
Alex O. Holcombe <aoholcombe@gmail.com>
2014-12-03 20:37:01 +1100

    playing with analyzing data in more psychopy-ish fashion

---
+++ analyzeData.py
@@ -0,0 +1,22 @@
+from psychopy import tools
+import inspect
+import psychopy_ext
+
+all_functions = inspect.getmembers(psychopy_ext, inspect.isfunction)
+print all_functions
+#grab some data outputted from my program, so I can test some analysis code
+##The psydat file format is literally just a pickled copy of the TrialHandler object that saved it. You can open it with:
+##dat = tools.filetools.fromFile(dataFileName)
+dataFileName = "data/Hubert_spatiotopicMotion_03Dec2014_15-49.psydat"
+dat = tools.filetools.fromFile(dataFileName)
+type(dat) #<class 'psychopy.data.DataHandler'>
+#Now I can test aggregate
+#dat.data seems to contain the columns I added
+dat.printAsText()
+
+d= dat.data #<class 'psychopy.data.DataHandler'>
+
+#test function fitting
+
+#have to use psychopy_ext to aggregate
+psychopy_ext.stats.aggregate(df, rows=None, cols=None, values=None, subplots=None, yerr=None, aggfunc='mean', unstacked=False, order='natural')
